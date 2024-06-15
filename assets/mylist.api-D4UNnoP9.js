4b6e9e3862a692e6f62b120dea879658806c69f6
tbs01215 <tbs01215@gmail.com>
2024-06-15 21:39:39 +0900

    Updates

---
+++ assets/mylist.api-D4UNnoP9.js
@@ -0,0 +1,1 @@
+import{A as t}from"./index-CiJ1Lp8P.js";const n=async s=>(await t.post("/mylist",s)).data,o=async()=>(await t.get("/mylist")).data,r=async s=>(await t.get(`/mylist/${s}`)).data,c=async s=>(await t.delete(`/mylist/${s}`)).data;export{r as a,n as c,c as d,o as g};
