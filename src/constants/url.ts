09bf2d63ce7672d21c51fbe0abc22f899cf4669f
tbs01215 <tbs01215@gmail.com>
2024-06-16 11:13:10 +0900

    baseurl변경

--- src/constants/url.ts
+++ src/constants/url.ts
@@ -1,3 +1,2 @@
-export const BASE_URL =
-  "https://memotube-fc8yz87s6-javascriptbiggosoos-projects.vercel.app";
+export const BASE_URL = "https://back-six-henna.vercel.app/";
 // export const BASE_URL = "http://localhost:8080";
